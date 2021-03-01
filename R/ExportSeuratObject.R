aa6f4688378f2f45eac92dc67b31c028cfea0b23
trile965 <lequangminhtri@gmail.com>
2021-03-01 18:09:32 +0800

    title

--- R/ExportSeuratObject.R
+++ R/ExportSeuratObject.R
@@ -9,7 +9,6 @@
 #' @import Matrix
 #' @import rhdf5
 #' @importFrom jsonlite write_json
-#' @importFrom uuid UUIDgenerate
 #' @importFrom zip zip
 #' @importFrom methods as
 #' @export
@@ -47,33 +46,6 @@ ExportSeuratObject <- function(
     return(data)
   }
 
-  WriteH5Matrix <- function(m, group, need.transpose=FALSE) {
-    rhdf5::h5createGroup(h5, group)
-    if (need.transpose) {
-      m <- Matrix::t(m)
-    }
-    rhdf5::h5write(m@x, h5, paste0(group, "/data"))
-    rhdf5::h5write(m@p, h5, paste0(group, "/indptr"))
-    rhdf5::h5write(m@i, h5, paste0(group, "/indices"))
-    rhdf5::h5write(dim(m), h5, paste0(group, "/shape"))
-    rhdf5::h5write(colnames(m), h5, paste0(group, "/barcodes"))
-    if (need.transpose) {
-      rhdf5::h5write(rownames(m), h5, paste0(group, "/features"))
-    } else {
-      rhdf5::h5write(feature.name, h5, paste0(group, "/features"))
-      rhdf5::h5write(feature.type, h5, paste0(group, "/feature_type"))
-    }
-  }
-
-  CreateCommit <- function() {
-    return(list(
-      hash_id = uuid::UUIDgenerate(),
-      created_at = as.numeric(Sys.time()) * 1000,
-      created_by = author,
-      description = "Converted with rBCS"
-    ))
-  }
-
   ValidateInput <- function() {
     if (length(object@reductions) == 0) {
       stop("Seurat object has no dimensionality reduction")
@@ -94,6 +66,37 @@ ExportSeuratObject <- function(
     }
   }
 
+  GetExpressionData <- function(object) {
+    omics <- names(object@assays)
+    counts <- GetData(object@assays, "RNA", c("counts", "data"))
+    norms <- GetData(object@assays, "RNA", "data")
+    feature.type <- rep("RNA", nrow(norms))
+    feature.name <- rownames(norms)
+    if ("ADT" %in% omics) {
+      counts <- Matrix::rbind2(counts, GetData(object@assays, "ADT", c("counts", "data")))
+      norms <- Matrix::rbind2(norms, GetData(object@assays, "ADT", "data"))
+      feature.type <- c(feature.type, rep("ADT", nrow(norms) - length(feature.type)))
+      feature.name <- rownames(norms)
+      feature.name[feature.type == "ADT"] <- paste("ADT", feature.name[feature.type == "ADT"], sep="-")
+    }
+    return(list(counts=counts, norms=norms, feature_type=feature.type))
+  }
+
+  GetDimredData <- function(object) {
+    data <- lapply(object@reductions, function(dimred) {
+      info <- list(
+        param = list(omics=dimred@assay.used),
+        coords = as.matrix(dimred@cell.embeddings)
+      )
+      if (ncol(info$coords) > 3) {
+        info$coords <- info$coords[, 1:3]
+      }
+      return(info)
+    })
+    names(data) <- names(object@reductions)
+    return(data)
+  }
+
   ValidateInput()
 
   Meow("Initializing...")
@@ -106,146 +109,10 @@ ExportSeuratObject <- function(
   dir.create(file.path(hash, "main"), recursive=TRUE)
 
   Meow("Extracting expressions...")
-  omics <- names(object@assays)
-  counts <- GetData(object@assays, "RNA", c("counts", "data"))
-  norms <- GetData(object@assays, "RNA", "data")
-  feature.type <- rep("RNA", nrow(norms))
-  feature.name <- rownames(norms)
-  if ("ADT" %in% omics) {
-    counts <- Matrix::rbind2(counts, GetData(object@assays, "ADT", c("counts", "data")))
-    norms <- Matrix::rbind2(norms, GetData(object@assays, "ADT", "data"))
-    feature.type <- c(feature.type, rep("ADT", nrow(norms) - length(feature.type)))
-    feature.name <- rownames(norms)
-    feature.name[feature.type == "ADT"] <- paste("ADT", feature.name[feature.type == "ADT"], sep="-")
-  }
-
-  Meow("Writing column sums...")
-  h5.path <- file.path(hash, "main", "matrix.hdf5")
-  rhdf5::h5createFile(h5.path)
-  h5 <- rhdf5::H5Fopen(h5.path)
-  rhdf5::h5createGroup(h5, "colsum")
-  raw.sum <- Matrix::colSums(counts)
-  rhdf5::h5write(raw.sum, h5, "colsum/raw")
-  rhdf5::h5write(log2(raw.sum + 1), h5, "colsum/log")
-  rhdf5::h5write(Matrix::colSums(norms), h5, "colsum/lognorm")
-
-  Meow("Writing matrices...")
-  WriteH5Matrix(counts, "bioturing")
-  WriteH5Matrix(counts, "countsT", need.transpose=TRUE)
-  WriteH5Matrix(norms, "normalizedT", need.transpose=TRUE)
-  rhdf5::H5Fclose(h5)
-  writeLines(feature.name, file.path(hash, "main", "genes.tsv"))
-  writeLines(colnames(norms), file.path(hash, "main", "barcodes.tsv"))
-  
-  Meow("Writing metadata...")
-  meta.dir <- file.path(hash, "main", "metadata")
-  dir.create(meta.dir)
-  meta.list <- list(version=1)
-  meta.list$content <- lapply(seq(ncol(object@meta.data)), function(i) {
-    info <- list(
-      id = uuid::UUIDgenerate(),
-      name = colnames(object@meta.data)[i],
-      type = if (is.numeric(object@meta.data[[i]])) "numeric" else "category",
-      clusters = as.character(object@meta.data[[i]]),
-      clusterName = "NaN",
-      clusterLength = 0,
-      history = list(CreateCommit())
-    )
-
-    # Use existing clustering result
-    if (info$name == clustering.name) {
-      info$type <- "category"
-      info$name <- "Graph-based clusters"
-      info$id <- "graph_based"
-      if (!any(is.na(as.numeric(info$clusters)))) {
-        info$clusters <- paste("Cluster", as.numeric(info$clusters))
-      }
-    }
-
-    # Category counting
-    if (info$type == "category") {
-      info$clusters[is.na(info$clusters)] <- "Unassigned"
-      info$clusterName <- c("Unassigned", unique(info$clusters))
-      if (length(info$clusterName) > unique.limit) {
-        warning(paste("Bad metadata -", info$name))
-        info$bad <- TRUE
-        return(info)
-      }
-      info$clusterLength <- sapply(info$clusterName, function(x) sum(info$clusters == x))
-      info$clusters <- match(as.character(info$clusters), info$clusterName) - 1 # base-0
-    }
-
-    jsonlite::write_json(info, file.path(meta.dir, paste0(info$id, ".json")), auto_unbox=TRUE)
-    info$clusters <- NULL # metalist.json does not need this info
-    return(info)
-  })
-  meta.list$content <- meta.list$content[sapply(meta.list$content, function(x) is.null(x$bad))]
-  names(meta.list$content) <- sapply(meta.list$content, function(x) x$id)
-  
-  # Add a fake graph-based result. TODO: Recognize graph-based result from object
-  if (is.null(meta.list$content$graph_based)) {
-    graph <- list(
-      id = "graph_based",
-      name = "Graph-based clusters",
-      type = "category",
-      clusters = rep(1, ncol(norms)),
-      clusterName = c("Unassigned", "Cluster 1"),
-      clusterLength = c(0, ncol(norms)),
-      history = list(CreateCommit())
-    )
-    jsonlite::write_json(graph, file.path(meta.dir, "graph_based.json"), auto_unbox=TRUE)
-    graph$clusters <- NULL
-    meta.list$content$graph_based <- graph
-  }
-
-  jsonlite::write_json(meta.list, file.path(meta.dir, "metalist.json"), auto_unbox=TRUE)
-  
-  Meow("Writing cell embeddings...")
-  dimred.dir <- file.path(hash, "main", "dimred")
-  dir.create(dimred.dir)
-  dimred <- list(version=1)
-  dimred$data <- lapply(seq(length(object@reductions)), function(i) {
-    info <- list(
-      name = names(object@reductions)[i],
-      id = uuid::UUIDgenerate(),
-      param = list(omics=object@reductions[[i]]@assay.used),
-      coords = as.matrix(object@reductions[[i]]@cell.embeddings),
-      history = list(CreateCommit())
-    )
-    if (ncol(info$coords) > 3) {
-      info$coords <- info$coords[, 1:3]
-    }
-    info$size <- dim(info$coords)
-    jsonlite::write_json(info, file.path(dimred.dir, info$id), auto_unbox=TRUE)
-    info$coords <- NULL # meta does not need this info
-    return(info)
-  })
-  names(dimred$data) <- sapply(dimred$data, function(x) x$id)
-  jsonlite::write_json(dimred, file.path(dimred.dir, "meta"), auto_unbox=TRUE)
-  
-  Meow("Writing general information...")
-  run.info <- list(
-    species = "human",
-    omics = as.list(unique(feature.type)), # Must be list
-    hash_id = hash,
-    version = 16,
-    n_cell = ncol(object),
-    addon = "SingleCell",
-    n_batch = 1,
-    platform = "unknown",
-    title = object@project.name,
-    unit = "umi",
-    author = list(),
-    abstract = "",
-    ana_setting = list(inputType="bcs"),
-    remote = list(),
-    history = list(CreateCommit()),
-    tag = list(),
-    shareTag = list(),
-    modified_date = as.numeric(Sys.time()) * 1000,
-    created_date = as.numeric(Sys.time()) * 1000
-  )
-  jsonlite::write_json(run.info, file.path(hash, "run_info.json"), auto_unbox=TRUE)
+  expr.data <- GetExpressionData(object)
+  dimred.data <- GetDimredData(object)
+  WriteStudy(expr.data, object@meta.data, dimred.data, hash,
+      clustering.name, author, unique.limit)
 
   Meow("Compressing data...")
   zip::zip(bcs.path, hash, compression_level=compression.level)
---
+++ R/WriteStudy.R
@@ -0,0 +1,166 @@
+#' @title
+#' @param expr.data list of counts, norms, and feature_type
+#' @param metadata a data.frame of metadata
+#' @param dimred.data a named list of dimensionality reductions
+#' @param study.path path to study directory
+#' @param clustering.name name of metadata that indicates clustering result
+#' @param author email of the creator
+#' @importFrom uuid UUIDgenerate
+WriteStudy <- function(expr.data, metadata, dimred.data, study.path, clustering.name, author, unique.limit) {
+  CreateCommit <- function() {
+    return(list(
+      hash_id = uuid::UUIDgenerate(),
+      created_at = as.numeric(Sys.time()) * 1000,
+      created_by = author,
+      description = "Converted with rBCS"
+    ))
+  }
+
+  WriteH5Matrix <- function(matrix, h5, group) {
+    rhdf5::h5createGroup(h5, group)
+    rhdf5::h5write(matrix@x, h5, paste0(group, "/data"))
+    rhdf5::h5write(matrix@p, h5, paste0(group, "/indptr"))
+    rhdf5::h5write(matrix@i, h5, paste0(group, "/indices"))
+    rhdf5::h5write(dim(matrix), h5, paste0(group, "/shape"))
+    rhdf5::h5write(colnames(matrix), h5, paste0(group, "/barcodes"))
+    rhdf5::h5write(rownames(matrix), h5, paste0(group, "/features"))
+  }
+
+  WriteExpressionData <- function(expr.data, study.path) {
+    Meow("Writing column sums...")
+    h5.path <- file.path(study.path, "main", "matrix.hdf5")
+    rhdf5::h5createFile(h5.path)
+    h5 <- rhdf5::H5Fopen(h5.path)
+    rhdf5::h5createGroup(h5, "colsum")
+    raw.sum <- Matrix::colSums(expr.data$counts)
+    rhdf5::h5write(raw.sum, h5, "colsum/raw")
+    rhdf5::h5write(log2(raw.sum + 1), h5, "colsum/log")
+    rhdf5::h5write(Matrix::colSums(expr.data$norms), h5, "colsum/lognorm")
+    Meow("Writing matrices...")
+    WriteH5Matrix(expr.data$counts, h5, "bioturing")
+    WriteH5Matrix(Matrix::t(expr.data$counts), h5, "countsT")
+    WriteH5Matrix(Matrix::t(expr.data$norms), h5, "normalizedT")
+    rhdf5::H5Fclose(h5)
+    writeLines(rownames(expr.data$norms), file.path(study.path, "main", "genes.tsv"))
+    writeLines(colnames(expr.data$norms), file.path(study.path, "main", "barcodes.tsv"))
+  }
+
+  WriteMetadata <- function(metadata, study.path, clustering.name) {
+    Meow("Writing metadata...")
+    meta.dir <- file.path(study.path, "main", "metadata")
+    dir.create(meta.dir)
+    meta.list <- list(version=1)
+    meta.list$content <- lapply(seq(ncol(metadata)), function(i) {
+      info <- list(
+        id = uuid::UUIDgenerate(),
+        name = colnames(metadata)[i],
+        type = if (is.numeric(metadata[[i]])) "numeric" else "category",
+        clusters = as.character(metadata[[i]]),
+        clusterName = "NaN",
+        clusterLength = 0,
+        history = list(CreateCommit())
+      )
+
+      # Use existing clustering result
+      if (info$name == clustering.name) {
+        info$type <- "category"
+        info$name <- "Graph-based clusters"
+        info$id <- "graph_based"
+        if (!any(is.na(as.numeric(info$clusters)))) {
+          info$clusters <- paste("Cluster", as.numeric(info$clusters))
+        }
+      }
+
+      # Category counting
+      if (info$type == "category") {
+        info$clusters[is.na(info$clusters)] <- "Unassigned"
+        info$clusterName <- c("Unassigned", unique(info$clusters))
+        if (length(info$clusterName) > unique.limit) {
+          warning(paste("Bad metadata -", info$name))
+          info$bad <- TRUE
+          return(info)
+        }
+        info$clusterLength <- sapply(info$clusterName, function(x) sum(info$clusters == x))
+        info$clusters <- match(as.character(info$clusters), info$clusterName) - 1 # base-0
+      }
+
+      jsonlite::write_json(info, file.path(meta.dir, paste0(info$id, ".json")), auto_unbox=TRUE)
+      info$clusters <- NULL # metalist.json does not need this info
+      return(info)
+    })
+    meta.list$content <- meta.list$content[sapply(meta.list$content, function(x) is.null(x$bad))]
+    names(meta.list$content) <- sapply(meta.list$content, function(x) x$id)
+    
+    # Add a fake graph-based result. TODO: Recognize graph-based result from object
+    if (is.null(meta.list$content$graph_based)) {
+      graph <- list(
+        id = "graph_based",
+        name = "Graph-based clusters",
+        type = "category",
+        clusters = rep(1, ncol(norms)),
+        clusterName = c("Unassigned", "Cluster 1"),
+        clusterLength = c(0, ncol(norms)),
+        history = list(CreateCommit())
+      )
+      jsonlite::write_json(graph, file.path(meta.dir, "graph_based.json"), auto_unbox=TRUE)
+      graph$clusters <- NULL
+      meta.list$content$graph_based <- graph
+    }
+
+    jsonlite::write_json(meta.list, file.path(meta.dir, "metalist.json"), auto_unbox=TRUE)
+  }
+
+  WriteDimred <- function(dimred.data, study.path) {
+    Meow("Writing cell embeddings...")
+    dimred.dir <- file.path(study.path, "main", "dimred")
+    dir.create(dimred.dir)
+    dimred <- list(version=1)
+    dimred$data <- lapply(seq(length(dimred.data)), function(i) {
+      info <- list(
+        name = names(dimred.data)[i],
+        id = uuid::UUIDgenerate(),
+        param = dimred.data[[i]]$param,
+        coords = dimred.data[[i]]$coords,
+        history = list(CreateCommit())
+      )
+      info$size <- dim(info$coords)
+      jsonlite::write_json(info, file.path(dimred.dir, info$id), auto_unbox=TRUE)
+      info$coords <- NULL # meta does not need this info
+      return(info)
+    })
+    names(dimred$data) <- sapply(dimred$data, function(x) x$id)
+    jsonlite::write_json(dimred, file.path(dimred.dir, "meta"), auto_unbox=TRUE)
+  }
+
+  WriteRunInfo <- function(study.path, omics, n.cell) {
+    Meow("Writing general information...")
+    run.info <- list(
+      species = "human",
+      omics = as.list(omics), # Must be list
+      hash_id = basename(study.path),
+      version = 16,
+      n_cell = n.cell,
+      addon = "SingleCell",
+      n_batch = 1,
+      platform = "unknown",
+      title = object@project.name,
+      unit = "umi",
+      author = list(),
+      abstract = "",
+      ana_setting = list(inputType="bcs"),
+      remote = list(),
+      history = list(CreateCommit()),
+      tag = list(),
+      shareTag = list(),
+      modified_date = as.numeric(Sys.time()) * 1000,
+      created_date = as.numeric(Sys.time()) * 1000
+    )
+    jsonlite::write_json(run.info, file.path(study.path, "run_info.json"), auto_unbox=TRUE)
+  }
+
+  dir.create(file.path(study.path, "main"), recursive=TRUE)
+  WriteExpressionData(expr.data, study.path)
+  WriteMetadata(metadata, study.path, clustering.name)
+  WriteDimred(dimred.data, study.path)
+  WriteRunInfo(study.path, unique(expr.data$feature.type), ncol(expr.data$norms))
+}
