bcdf476a747b83a86d0f6532e8d366c42abca5a6
Duy Tan <ptdtan@gmail.com>
2021-03-14 00:01:55 +0700

    add pre-built binary and the installation guide

---
+++ src/install.libs.R
@@ -0,0 +1,21 @@
+# Detect the OS: Linux, Windows, or Darwin
+os <- Sys.info()[['sysname']]
+
+# The installation destination. Please use the same way to load the binary
+dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
+dir.create(dest, recursive = TRUE, showWarnings = TRUE)
+
+if (os == "Linux") {
+	files <- file.path(R_PACKAGE_SOURCE, "src/bcs_transpose.pyx.dist.linux")
+} else if (os == "Windows") {
+	files <- file.path(R_PACKAGE_SOURCE, "src/bcs_transpose.pyx.dist.win")
+} else if (os == "Darwin") {
+	files <- file.path(R_PACKAGE_SOURCE, "src/bcs_transpose.pyx.dist.mac")
+}
+
+# Copy file
+message("Copying files from ", files, " to ", dest)
+message(paste(list.files(files), sep = "\n"))
+file.copy(files, dest, overwrite = TRUE, recursive = T, )
+
+
