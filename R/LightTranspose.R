a46ccfe8a644a101abda0c27b6cf3879f807c7f7
Duy Tan <ptdtan@gmail.com>
2021-03-14 17:20:49 +0700

    change the function interface

--- R/LightTranspose.R
+++ R/LightTranspose.R
@@ -1,22 +1,38 @@
 #' @title Tranpose a CSC sparse matrix with low memory and high speed 
-#' @param input_path Input path of the hdf5 matrix 
-#' @param output_path Path of the hdf5 matrix 
+#' @param hdf5.path Input path of the hdf5 matrix. Will expand this matrix file
 #' @param chunk_size Divide the matrix into equal chunks. It should be the square root of number of columns
 #' @export
 LightTranspose <- function(
-  input.path,
-  output.path,
+  hdf5.path,
   chunk_size = 100
   ) {
   
-  light.tranpose.bin <- file.path(find.package("rBCS"), "lib", "light_transpose.pyx.dist", 
-                                  "light_transpose.pyx")
-  if (! file.exists(light.tranpose.bin)) {
-    stop("the backend library of light transpose is not available. Your rBCS installation might be broken. Please try to install it again.")
+  GetBinary <- function() {
+    os <- Sys.info()[['sysname']]
+    r_arch <-.Platform $ r_arch
+    
+    if (os == "Linux") {
+      transpose.bin <- system.file("libs", r_arch, "bcs_transpose.pyx.dist.linux/bcs_transpose.pyx", package = "rBCS")
+    } else if (os == "Windows") {
+      transpose.bin <- system.file("libs", r_arch, "bcs_transpose.pyx.dist.win/bcs_transpose.pyx.exe", package = "rBCS")
+      
+    } else if (os == "Darwin") {
+      transpose.bin <- system.file("libs", r_arch, "bcs_transpose.pyx.dist.mac/bcs_transpose.pyx", package = "rBCS")
+    }
+    return(transpose.bin)
+  }
+  
+  transpose.bin <- GetBinary()
+  
+  if (transpose.bin == "") {
+    stop("the backend library of the bcs file transpose is not available. Your rBCS installation might be broken. Please try to install it again.")
   }
   message("Now transpose the matrix...")
-  ret <- system(paste(light.tranpose.bin, input.path, output.path, chunk_size))
+  cmd <- paste0('"',transpose.bin,'"', ' ' , '"', hdf5.path, '"', ' ', chunk_size)
+  message(cmd)
+  ret <- system(cmd)
+  message(ret)
   if (ret)
     stop("Some error happened, Please post an issue here to report it: https://github.com/bioturing/rBCS/issues")
-  message(paste("Transpose successfully, your output hdf5 file is located at", output.path ))
+  message(paste("Transpose successfully, your output hdf5 file is located at", hdf5.path ))
 }
